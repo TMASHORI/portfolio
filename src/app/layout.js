2106a9c1c7244cb487b70410eb3fb1a9c18cbca3
THATOMAS <chachumashori@gmail.com>
2024-09-14 10:31:25 +0200

    Moved the arrow button on the landing view .

--- src/app/layout.js
+++ src/app/layout.js
@@ -16,6 +16,7 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <head>
         <link rel="icon" href="/favicon.ico" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
       </head>
       <body className={styles.inter}>
         {children}
